b3066435f1f54a1f1dc28ed62df9e7a032a677f9
denoolen <87325015+denoolen@users.noreply.github.com>
2021-10-18 01:12:46 +0300

    Update

---
+++ Teams.Client/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.Client.MVVM.Model;
+
+namespace Teams.Client.MVVM.ViewModel
+{
+	internal class MainViewModel
+	{
+		
+	}
+}
--- Teams.Client/MainWindow.xaml.cs
+++ Teams.Client/MainWindow.xaml.cs
@@ -12,17 +12,103 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Teams.Client.ServiceChat;
 
 namespace Teams.Client
 {
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, IServiceChatCallback
 	{
+		bool IsConnected = false;
+		int ID;
+		ServiceChatClient client;
+		
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+		void ConnectUser()
+		{
+			
+			if (!IsConnected)
+			{
+				client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+				ID = client.Connect(searchTextBox.Text);
+				searchTextBox.IsEnabled = false;
+				TBConnect.Text = "Disconnect";
+				IsConnected = true;
+			}
+		}
+		void DisconnectUser()
+		{
+			if (IsConnected)
+			{
+				client.Disconnect(ID);
+				client = null;
+				searchTextBox.IsEnabled = true;
+				TBConnect.Text = "Connect";
+				IsConnected = false;
+			}
+		}
+
+		private void Head_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if(e.LeftButton == MouseButtonState.Pressed)
+			{
+				DragMove();
+			}
+		}
+
+		private void ButtonMinimaze_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void ButtonShutdown_Click(object sender, RoutedEventArgs e)
+		{
+			DisconnectUser();
+			Application.Current.Shutdown();
+		}
+
+		private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+		{
+			if(Application.Current.MainWindow.WindowState != WindowState.Maximized)
+				Application.Current.MainWindow.WindowState= WindowState.Maximized;
+			else
+				Application.Current.MainWindow.WindowState = WindowState.Normal;
+		}
+
+		private void ConnectUser(object sender, RoutedEventArgs e)
+		{
+			if (IsConnected)
+			{
+				DisconnectUser();
+			}
+			else
+			{
+				ConnectUser();
+			}
+
+		}
+
+		public void MsgCallback(string msg)
+		{
+			LbChat.Items.Add(msg);
+		}
+
+		private void SendTextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.Key == Key.Enter)
+			{
+				if (client != null)
+				{
+					client.SendMsg(searchTextBox.Text, ID);
+					SendTextBox.Text = String.Empty;	
+				}
+				
+			}
 		}
 	}
 }
